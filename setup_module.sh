7555b5fb98aa3487c9829dc59a92483332c3c787
dbzho <parksm@com2us.com>
2024-04-05 12:36:01 +0900

    test

--- setup_module.sh
+++ setup_module.sh
@@ -12,64 +12,6 @@ echo "brew install terraform-docs"
 echo "4.standard version && conventional commit setting"
 echo "brew install jq"
 yarn init -y
-npx husky-init && yarn
-rm -rf .husky/*
-cat <<EOF > .husky/commit-msg
-#!/usr/bin/env sh
-. "$(dirname -- "$0")/_/husky.sh"
-
-npx --no-install commitlint --edit $1
-EOF
-
-yarn add -D @commitlint/cli @commitlint/config-conventional
-echo "module.exports = { extends: ['@commitlint/config-conventional'] };" > commit.config.js
-npx husky add .husky/commit-msg 'npx --no-install commitlint --edit $1'
-cat <<EOF > .versionrc
-{
-    "types": [
-        {
-            "type": "feat",
-            "section": "Features",
-            "hidden": false
-        },
-        {
-            "type": "fix",
-            "section": "Bug Fixes",
-            "hidden": false
-        },
-        {
-            "type": "test",
-            "section": "Tests",
-            "hidden": true
-        },
-        {
-            "type": "build",
-            "section": "Build System",
-            "hidden": true
-        },
-        {
-            "type": "chore",
-            "section": "Others",
-            "hidden": false
-        },
-        {
-            "type": "docs",
-            "section": "Docs",
-            "hidden": true
-        },
-        {
-            "type": "style",
-            "section": "Styling",
-            "hidden": false
-        },
-        {
-            "type": "refactor",
-            "section": "Code Refactoring",
-            "hidden": false
-        }
-    ]
-}
-EOF
 
 yarn add -D standard-version
 json=$(cat package.json)
@@ -78,3 +20,6 @@ echo "$new_json" > package.json
 echo "Scripts added to package.json."
 
 yarn release -- --first-release
+git config --unset-all core.hooksPath
+pre-commit install
+pre-commit install --hook-type commit-msg
