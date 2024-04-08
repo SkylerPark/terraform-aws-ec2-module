1ef94f3b09e107875a9e569a2259aceedb3975cb
dbzho <parksm@com2us.com>
2024-04-08 10:04:41 +0900

    refactor: setup module script 오타 수정

--- setup_module.sh
+++ setup_module.sh
@@ -1,6 +1,6 @@
 ### MAC Install
 echo "Package Install"
-echo "Package pre-commit, tfenv, terraform-docs, jq, tflinut, tfsec"
+echo "Package pre-commit, tfenv, terraform-docs, jq, tflint, tfsec"
 package_list="pre-commit tfenv terraform-docs jq tflint tfsec"
 
 for package in $package_list; do
