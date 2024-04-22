a1dc3652fee92c12c30ab7fed0649edf746830b5
SkylerPark <012tjdals012@gmail.com>
2024-04-22 19:23:45 +0900

    fix: key pair output update

--- modules/key-pair/outputs.tf
+++ modules/key-pair/outputs.tf
@@ -12,3 +12,9 @@ output "public_key" {
   description = "key pair public key"
   value       = aws_key_pair.this.public_key
 }
+
+output "private_key_pem" {
+  description = "Private key data PEM (RFC 1421)"
+  value       = tls_private_key.this[0].private_key_pem
+  sensitive   = true
+}
